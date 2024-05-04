40fad42538312866f7241d78435f3d255e39bb0e
Qujuan Miller <qujuan.miller@snhu.edu>
2024-05-04 17:04:26 -0400

    seo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-
-import { render } from 'react-snapshot';
+import React from 'react';
+import { hydrate, render } from "react-dom";
+import './index.css';
 import App from './App';
- 
 
-render(
-    <App/>,
-    document.getElementById('root')
-  );
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
