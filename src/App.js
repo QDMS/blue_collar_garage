648da33b84e9f5da31562d508f856733ab73b433
Qujuan Miller <qujuan.miller@snhu.edu>
2024-05-04 17:23:43 -0400

    seo meta to app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Services } from "./components/Services";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
+   
     <div className="App">
+       <Helmet>
+        <meta title="Blue Collar Garage"/>
+        <meta name=" Blue Collar Garage A Licensed And Insured Mechanic Shop" content="Auto Repair Shop"/>
+       </Helmet>
       <NavBar />
       <Banner />
       <Services />
